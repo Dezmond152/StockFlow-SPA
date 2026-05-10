4d535420b21056125d8cd293a24c55d7304be073
Dezmond152 <rossomahavit@gmail.com>
2026-05-10 19:07:55 +0300

    feat: order page + order item + svg icons

---
+++ frontend/src/icons/AddIcon.jsx
@@ -0,0 +1,17 @@
+export function AddIcon({ size = 16, color = "currentColor" }) {
+  return (
+    <svg 
+      width={size} 
+      height={size} 
+      viewBox="0 0 24 24" 
+      fill="none" 
+      stroke={color} 
+      strokeWidth="3" 
+      strokeLinecap="round" 
+      strokeLinejoin="round"
+    >
+      <line x1="12" y1="5" x2="12" y2="19"></line>
+      <line x1="5" y1="12" x2="19" y2="12"></line>
+    </svg>
+  );
+}
--- frontend/src/pages/Orders/Orders.jsx
+++ frontend/src/pages/Orders/Orders.jsx
@@ -1,3 +1,25 @@
-export function Orders (){
-  return <div>Orders Page</div>
+import "./Orders.css";
+import { OrderItem } from "../../components/OrderItem/OrderItem";
+import { AddIcon } from '../../icons/AddIcon';
+
+export function Orders() {
+  return (
+    <section className="orders container orders--centered">
+      
+      <header className="orders__header">
+        <button className="orders__add-btn">
+          <AddIcon size={20} color="white" />
+        </button>
+        <h1 className="orders__title">Orders / 25</h1>
+      </header>
+
+      <div className="orders__list">
+        <OrderItem />
+        <OrderItem />
+        <OrderItem />
+        <OrderItem />
+      </div>
+      
+    </section>
+  );
 }
