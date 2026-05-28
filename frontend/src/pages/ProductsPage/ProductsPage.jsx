e77e341f466e0b2d9b52344409280aed37f841f3
Dezmond152 <rossomahavit@gmail.com>
2026-05-28 20:25:44 +0300

    feat: added filter logic

--- frontend/src/pages/ProductsPage/ProductsPage.jsx
+++ frontend/src/pages/ProductsPage/ProductsPage.jsx
@@ -3,10 +3,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../../store/productsSlice";
 import { ProductCard } from "../../components/ProductCard/ProductCard";
 import "./ProductsPage.css";
+import { useState } from "react";
 
 export function ProductsPage() {
   const dispatch = useDispatch();
-
+  const [filter, setFilter] = useState("Все");
   const { items: products, status, error } = useSelector((state) => state.products);
 
   useEffect(() => {
@@ -26,19 +27,28 @@ export function ProductsPage() {
   const allTypes = products.map((item) => item?.type);
   const validTypes = allTypes.filter((type) => type !== undefined);
   const productTypeArr = [...new Set(validTypes)];
-  // console.log(productTypeArr)
+
+  const handleChange = (event) => {
+    const selectedTypeIndex = event.target.options.selectedIndex;
+    const selectedType = event.target.options[selectedTypeIndex].text;
+    setFilter(selectedType);
+  };
+
+  const filteredProducts = filter === "Все" ? products : products.filter((item) => item.type === filter);
 
   return (
     <section className="products container-fluid">
       <header className="products__header">
-        <h1 className="products__title">Продукты / {products.length}</h1>
+        <h1 className="products__title">Продукты / {filteredProducts.length}</h1>
 
         <div className="products__filters">
           <div className="products__filter-group">
             <label className="products__filter-label">Тип:</label>
-            <select className="products__filter-select">
+            <select className="products__filter-select" onChange={handleChange}>
               <option>Все</option>
-              {productTypeArr.map((item) => <option key={item}>{item}</option>)}
+              {productTypeArr.map((item) => (
+                <option key={item}>{item}</option>
+              ))}
             </select>
           </div>
         </div>
@@ -46,7 +56,7 @@ export function ProductsPage() {
 
       <div className="products__content-wrapper">
         <div className="products__list">
-          {products.map((item) => (
+          {filteredProducts.map((item) => (
             <ProductCard key={item.id} product={item} onDelete={(id) => dispatch(deleteProduct(id))} />
           ))}
         </div>
